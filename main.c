e67ff6c3303a7e2f7b541e73b642cca15316cf2c
alphaOri <proudtobeagrover@gmail.com>
2012-03-11 17:15:07 -0400

    Switches turn directly corresponding leds on.
    'select_mode' function added.

--- main.c
+++ main.c
@@ -9,7 +9,37 @@ Date: 9/9/2011
 #include <pic.h>
  
 //FOSC_INTOSCIO -> use internal oscillator and let CLKIN & CLKOUT be digital I/O
+//MCLRE_OFF -> use RA3 as digital input rather than MCLR
 __CONFIG(FOSC_INTOSCIO & PWRTE_OFF & BOREN_OFF & CP_OFF & CPD_OFF & WDTE_OFF & MCLRE_OFF & IESO_OFF & FCMEN_OFF);
+
+
+/////////////////DEFINE GLOBAL VARIABLES///////////////////
+
+#define TRUE 1
+#define FALSE 0
+
+typedef union
+{
+	unsigned char byte;
+	struct{
+		unsigned bit0:1;
+		unsigned bit1:1;
+		unsigned bit2:1;
+		unsigned bit3:1;
+		unsigned bit4:1;
+		unsigned bit5:1;
+		unsigned bit6:1;
+		unsigned bit7:1;
+	}bits;	
+} bit_field;
+
+bit_field sw_state_new = {FALSE}, sw_state_old = {FALSE}, led_state = {FALSE};
+
+unsigned char mode = 0;
+
+/////////////////FUNCTION DECLARATIONS///////////////////
+
+void select_mode(void);
 
 void main()
 {
@@ -26,27 +56,37 @@ void main()
 	ANSEL = 0x00; //PORTA & PORTC assigned as digital I/O
 	CMCON0 = 0x07;
 	//WPUA = 0x00; //Disable weak pull-ups, our switch setup doesn't need them
-	IOCA = 0b000001; //set RA0 as interrupt-on-change
-	TRISA = 0x01; //set RA0 as inputs
+	IOCA = 0x0F; //set RA0, RA1, RA2, RA3 as interrupt-on-change
+	TRISA = 0x0F; //set RA0, RA1, RA2, RA3 as inputs
 
-	//Set PORTC as output
+	//Set PORTC as output for LEDs
 	PORTC = 0x00; //clear PORTC
 	TRISC = 0x00; //set all as outputs
 	
-
-
-	
 	//Initial blinking light show	
-	unsigned int i;	
-	unsigned int delay = 15000;
+//	unsigned int i;	
+//	unsigned int delay = 15000;
+//
+//	while(1==1)
+//	{
+//		NOP();
+//		for(i=0; i<delay; i++);
+//		NOP();
+//		RC1 = RC1 ^ 1;
+//	}	
 
-	while(1==1)
+	while(TRUE)
 	{
-		NOP();
-		for(i=0; i<delay; i++);
-		NOP();
-		RC1 = RC1 ^ 1;
-	}		
+		RC0 = led_state.bits.bit0;
+		RC1 = led_state.bits.bit1;
+		RC2 = led_state.bits.bit2;
+		RC3 = led_state.bits.bit3;
+//		RC0 = 0;
+//		RC1 = 1;
+//		RC2 = 0;
+//		RC3 = 1;
+	}
+
 }
 
 void interrupt sw_int(void)
@@ -81,9 +121,21 @@ void interrupt sw_int(void)
 	NOP();
 	for(j=0; j<500; j++);
 	NOP();
-	RC3 = PORTAbits.RA0;
+	led_state.bits.bit0 = RA0;
+	led_state.bits.bit1 = RA1;
+	led_state.bits.bit2 = RA2;
+	led_state.bits.bit3 = RA3;
 	INTCONbits.RAIF = 0;
 }
+
+void select_mode(void)
+{
+	sw_state_new.bits.bit0 = RA0;
+	sw_state_new.bits.bit1 = RA1;
+	sw_state_new.bits.bit2 = RA2;
+	sw_state_new.bits.bit3 = RA3;
+	mode = sw_state_new.byte;
+}	
 
 /*
 void button_debounce(void)
@@ -95,7 +147,7 @@ void button_debounce(void)
 			reset timer
 		elseif(timer < waitperiod)
 			timer++
-		elseif(timer >waitperiod)
+		elseif(timer > waitperiod)
 			debouncing = false
 	}
 }
